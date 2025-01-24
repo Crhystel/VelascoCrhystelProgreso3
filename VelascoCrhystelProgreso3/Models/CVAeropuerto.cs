cf42f7e4282a935e49a0f6c32c99e5e82bb9fe6b
Crhystel <crhystelvelasco213@gmail.com>
2025-01-24 07:22:26 -0500

    inicio del proyecto

---
+++ VelascoCrhystelProgreso3/Models/CVAeropuerto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VelascoCrhystelProgreso3.Models
+{
+    public class CVAeropuerto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
