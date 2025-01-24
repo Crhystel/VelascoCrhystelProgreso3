7bef10a76205731836296609dcb03f6540f02170
Crhystel <crhystelvelasco213@gmail.com>
2025-01-24 07:26:42 -0500

    inicio de consumo de api

---
+++ VelascoCrhystelProgreso3/Repositories/CVAeropuertoRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using VelascoCrhystelProgreso3.Interfaces;
+using VelascoCrhystelProgreso3.Models;
+
+namespace VelascoCrhystelProgreso3.Repositories
+{
+    public class CVAeropuertoRepository : IAeropuerto
+    {
+        private readonly HttpClient _httpClient;
+        public CVAeropuertoRepository()
+        {
+            _httpClient = new HttpClient();
+        }
+        public Task<CVAeropuerto> GetAeropuerto(string name)
+        {
+            try
+            {
+                var url = $"https://localhost:44300/api/Aeropuerto/{name}";
+                var response = _httpClient.GetAsync(url).Result;
+                return JsonSerializer.DeserializeAsync<CVAeropuerto>(response, new JsonSerializerOptions({
+                    PropertyNameCaseInsensitive = true
+                });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
